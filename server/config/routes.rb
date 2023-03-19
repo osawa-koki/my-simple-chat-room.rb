b90f9ad9dd6c6307d2ff88c692dc1b7b68c466c5
osawa-koki <github@osawa.dev>
2023-03-20 01:50:28 +0900

    小さな修正。

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'user/user'
-  get 'hello/hello'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
