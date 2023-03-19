a63398e49c8bc53c0cb6d9795394b3c63068faa6
osawa-koki <github@osawa.dev>
2023-03-20 00:47:16 +0900

    greet - test

--- server/test/controllers/greetings_controller_test.rb
+++ server/test/controllers/greetings_controller_test.rb
@@ -1,8 +1,35 @@
-# require "test_helper"
+require 'test_helper'
 
-# class GreetingsControllerTest < ActionDispatch::IntegrationTest
-#   test "should get greet" do
-#     get greetings_greet_url
-#     assert_response :success
-#   end
-# end
+class GreetingsControllerTest < ActionDispatch::IntegrationTest
+  test "should get greet with GET method" do
+    method_name = "GET"
+    get "/greet", params: { method_name: method_name }
+    assert_response :success
+    response_data = JSON.parse(response.body)
+    assert_equal "Hello #{method_name}.", response_data["message"]
+  end
+
+  test "should get greet with POST method" do
+    method_name = "POST"
+    post "/greet", params: { method_name: method_name }
+    assert_response :success
+    response_data = JSON.parse(response.body)
+    assert_equal "Hello #{method_name}.", response_data["message"]
+  end
+
+  test "should get greet with PUT method" do
+    method_name = "PUT"
+    put "/greet", params: { method_name: method_name }
+    assert_response :success
+    response_data = JSON.parse(response.body)
+    assert_equal "Hello #{method_name}.", response_data["message"]
+  end
+
+  test "should get greet with DELETE method" do
+    method_name = "DELETE"
+    delete "/greet", params: { method_name: method_name }
+    assert_response :success
+    response_data = JSON.parse(response.body)
+    assert_equal "Hello #{method_name}.", response_data["message"]
+  end
+end
