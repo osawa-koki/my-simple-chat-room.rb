8165c00a6e445e82e5ac664ebb7f62e8c80c5a4c
osawa-koki <github@osawa.dev>
2023-03-20 00:43:32 +0900

    Helloテスト。

--- server/test/controllers/hello_controller_test.rb
+++ server/test/controllers/hello_controller_test.rb
@@ -1,10 +1,13 @@
 require 'test_helper'
 
 class HelloControllerTest < ActionDispatch::IntegrationTest
-  test "should return hello message" do
-    name = "hogehoge"
-    get "/hello/#{name}"
-    assert_response :success
-    assert_equal({ message: "Hello #{name}" }.to_json, response.body)
+  data = ["hogehoge", "fugafuga", "piyopiyo"]
+
+  data.each do |name|
+    test "should return hello message for #{name}" do
+      get "/hello/#{name}"
+      assert_response :success
+      assert_includes response.body, "Hello #{name}"
+    end
   end
 end
