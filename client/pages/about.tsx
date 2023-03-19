10ca0a63706b48bcee54cb636e35947119f19cab
osawa-koki <github@osawa.dev>
2023-03-20 02:44:18 +0900

    About

--- client/pages/about.tsx
+++ client/pages/about.tsx
@@ -6,15 +6,11 @@ export default function AboutPage() {
     <Layout>
       <div id="About">
         <h1>Here, About page.</h1>
-        <p className="mt-3">
-          Next.js大好きです。<br />
-          本当に使いやすいです。<br />
-          僕はTypeScript × Next.jsを強くオススメします。<br />
+        <p className="mt-5">
+          `Ruby on Rails` × `Next.js`で作成したチャットアプリです。<br />
           <br />
-          静的型付け言語って難しいと思われがちですが、エディタの機能が豊富な現在は型付け言語の方が簡単に書くことができます。<br />
-          さらには字句解析と構文解析に加えて、コンパイル時に、コンパイラによる意味解析を行ってくれるため、動的型付け言語と比較して動作に信頼性があります。<br />
-          <br />
-          また、Reactと比べてNext.jsはSSGによってビルド時にHTMLが生成されるため、SPA(SSR)と比べて高速に動作し、ユーザビリティが高いです。
+          `Ruby on Rails`はAPIモードを使用し、`Next.js`は`TypeScript`で開発しました。<br />
+          転職先の企業で`Ruby on Rails` × `Next.js`を使用するので、その学習用です。
         </p>
       </div>
     </Layout>
