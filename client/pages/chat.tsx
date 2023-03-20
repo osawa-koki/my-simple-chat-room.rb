31f4d7feb424694d8bcafa147c47efd494f964d5
osawa-koki <github@osawa.dev>
2023-03-20 13:07:23 +0900

    パスを追加した。

--- client/pages/chat.tsx
+++ client/pages/chat.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-import { Button, Alert, Form } from 'react-bootstrap';
+import { Button, Alert, Form, Spinner } from 'react-bootstrap';
 import Layout from "../components/Layout";
 
 import { DataContext } from "../src/DataContext";
@@ -9,6 +9,33 @@ export default function ContactPage() {
 
   const { sharedData, setSharedData } = React.useContext(DataContext);
   const [message, setMessage] = useState<string>('');
+  const [socket, setSocket] = useState<WebSocket>();
+  const [ready, setReady] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const socket = new WebSocket('ws://localhost:8000/chat');
+    setSocket(socket);
+  }, []);
+
+  useEffect(() => {
+    if (socket) {
+      socket.onopen = () => {
+        setReady(true);
+        console.log('Connected to server');
+      };
+      socket.onmessage = (event) => {
+        console.log('Message from server ', event.data);
+      };
+      socket.onclose = (event) => {
+        console.log('Socket closed connection: ', event);
+      };
+      socket.onerror = () => {
+        setReady(true);
+        setError('Socket encountered an error.');
+      };
+    }
+  }, [socket]);
 
   return (
     <Layout>
@@ -27,8 +54,25 @@ export default function ContactPage() {
             <Form.Label>Message</Form.Label>
             <Form.Control type="text" placeholder="Enter message" value={message} onInput={(e) => {setMessage(e.currentTarget.value)}}/>
           </Form.Group>
-          <Button variant="primary" className="mt-3 d-block m-auto">Send 📨</Button>
+          <Button variant="primary" className="mt-3 d-block m-auto" disabled={ready === false}>Send 📨</Button>
         </Form>
+        {
+          error && <Alert variant="danger" className="mt-3">{error}</Alert>
+        }
+        {
+          ready === false && (
+            <div className="d-flex justify-content-around mt-3">
+              <Spinner animation="border" variant="primary" />
+              <Spinner animation="border" variant="secondary" />
+              <Spinner animation="border" variant="success" />
+              <Spinner animation="border" variant="danger" />
+              <Spinner animation="border" variant="warning" />
+              <Spinner animation="border" variant="info" />
+              <Spinner animation="border" variant="light" />
+              <Spinner animation="border" variant="dark" />
+            </div>
+          )
+        }
       </div>
     </Layout>
   );
